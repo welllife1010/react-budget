232bbfe0782f0353da521f8675dbcf39c7ef7c3f
Silvia Chen <silviachen@Silvia-Chens-MacBook-Pro-1292.local>
2024-02-27 22:34:59 -0800

    basic styles. context setup. custom hook setup.

--- src/context/BudgetsContext.jsx
+++ src/context/BudgetsContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext } from "react"
+import { createContext, useContext, useState } from "react"
 
 const BudgetsContext = createContext()
 
@@ -7,6 +7,42 @@ export function useBudgets() {
 }
 
 export const BudgetsProvider = ({ children }) => {
+  const [budgets, setBudgets] = useState([]) // {id, name, max}
+  const [expenses, setExpenses] = useState([]) // {id, budgetId, amount, description}
+
+  function getBudgetExpenses(budgetId) {
+    return expenses.filter((expense) => expense.budgetId === budgetId)
+  }
+
+  function addExpense({ description, amount, budgetId }) {
+    setExpenses((prevExpenses) => {
+      return [
+        ...prevExpenses,
+        { id: crypto.randomUUID, budgetId, amount, description },
+      ]
+    })
+  }
+
+  function addBudget({ name, max }) {
+    setBudgets((prevBudgets) => {
+      if (prevBudgets.find((budget) => budget.name === name)) return prevBudgets
+      return [...prevBudgets, { id: crypto.randomUUID, name, max }]
+    })
+  }
+
+  function deleteBudget({ id }) {
+    // TODO: Deal with expenses
+    setBudgets((prevBudgets) => {
+      return prevBudgets.filter((budget) => budget.id !== id)
+    })
+  }
+
+  function deletExpense({ id }) {
+    setExpenses((prevExpenses) => {
+      return prevExpenses.filter((expense) => expense.id !== id)
+    })
+  }
+
   return (
     <BudgetsContext.Provider
       value={{
